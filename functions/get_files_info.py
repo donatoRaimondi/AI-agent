91eaa25eabc4c18710b7f40d61896ca094ac50b7
donatoRaimondi <rikuraimondi@gmail.com>
2026-01-25 17:10:42 +0100

    Added new directories to project

---
+++ functions/get_files_info.py
@@ -0,0 +1,30 @@
+import os
+
+def get_files_info(working_directory, directory="."):
+    try:
+        # Resolve absolute paths
+        working_dir_abs = os.path.abspath(working_directory)
+        target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
+
+        # Security check: prevent directory traversal
+        valid_target_dir = (
+            os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
+        )
+        if not valid_target_dir:
+            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+
+        # # Check existence
+        # if not os.path.exists(target_dir):
+        #     return f'Error: "{directory}" does not exist'
+
+        # Check that it is a directory
+        if not os.path.isdir(target_dir):
+            return f'Error: "{directory}" is not a directory'
+
+        dir_content = ""
+        for item in os.listdir(target_dir):
+                dir_content = dir_content + f"- {item}: file_size:{os.path.getsize(os.path.join(target_dir, item))}, is_dir={os.path.isdir(os.path.join(target_dir, item))}\n"
+        
+        return dir_content
+    except:
+        return f"Error: something went wrong"
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 from google import genai
 from argparse import ArgumentParser
-
+from google.genai import types
 
 
 def main():
@@ -11,27 +11,35 @@ def main():
     if api_key is None:
         raise RuntimeError('Key has not been found')
     client = genai.Client(api_key=api_key)
-    prompt = parse_arguments()
+
+    args = parse_arguments()
+    prompt = args.user_prompt
+
+    messages = [types.Content(role="user", parts=[types.Part(text=prompt)])] 
+
     response = client.models.generate_content(
-        model='gemini-2.5-flash', contents=prompt
+        model='gemini-2.5-flash', contents=messages
     )
+
     if response.usage_metadata is None:
         raise RuntimeError('Usage Metadata not returned!')
-    model_response(prompt, response)
+
+    model_response(prompt, response, verbose=args.verbose)
 
     
-def model_response(prompt, response):
-    print(f'User prompt: {prompt}')
-    print(f'Prompt tokens: {response.usage_metadata.prompt_token_count}')
-    print(f'Response tokens: {response.usage_metadata.candidates_token_count}')
+def model_response(prompt, response, verbose=False):
+    if verbose:
+        print(f'User prompt: {prompt}')
+        print(f'Prompt tokens: {response.usage_metadata.prompt_token_count}')
+        print(f'Response tokens: {response.usage_metadata.candidates_token_count}')
     print(f'Response:\n{response.text} ')
 
 
 def parse_arguments():
     parser = ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
-    args = parser.parse_args()
-    return args.user_prompt
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    return parser.parse_args()
 
 if __name__ == "__main__":
     main()
